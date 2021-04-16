709251f42c6e0b4d043959e065e02c5f07f468a0
devkcodes <devanshkatheria27@gmail.com>
2021-04-16 22:53:47 +0530

    ready to deploy

--- server.js
+++ server.js
@@ -4,13 +4,15 @@ const connectDB = require("./config/db");
 
 const app = express();
 
+const path = require('path');
+
 connectDB(); //calls connectDB function in db.js 
 
 //Init middleware
 app.use(express.json({ extended: false }));
 
-//connect database
-app.get('/', (req, res) => res.send('API Running'));
+
+
 
 //Define routes
 app.use('/api/users', require('./routes/api/users'));
@@ -18,6 +20,15 @@ app.use('/api/auth', require('./routes/api/auth'));
 
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
+
+//static assets
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static("client/build"));
+
+    app.get('*',(req,res)=>{
+        res.sendFile(path.resolve(_dirname,'client','build','index.html'))
+    })
+}
 
 const PORT = process.env.PORT || 5000;
 
